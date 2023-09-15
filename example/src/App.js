2e05e20d520586f3d1a954d3f01b00a8111c5c15
dev-inikhiya <88757383+devichand-stan@users.noreply.github.com>
2023-09-16 00:09:08 +0530

    fixes

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,3 @@
-
 import {
   View,
   Text,
@@ -10,11 +9,13 @@ import {
   ScrollView,
   ActivityIndicator,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ImagePicker from 'react-native-image-crop-picker';
-import TesseractOcr, { useEventListener } from 'react-native-tessractocr';
+import TesseractOcr, {
+  useEventListener,
+} from '@devinikhiya/react-native-tesseractocr';
 
-const defaultPickerOptions = {cropping: true};
+const defaultPickerOptions = { cropping: true };
 export default function TessrectModule() {
   const [imgSrc, setimgSrc] = useState(null);
   const [resultText, setresultText] = useState();
@@ -25,17 +26,13 @@ export default function TessrectModule() {
   });
   useEffect(() => {
     //recoginze text from remote image url
-    recognizeTextFromImage('https://i.stack.imgur.com/0Jl54.png')
-  }, [])
-  
-  const recognizeTextFromImage = async path => {
+    recognizeTextFromImage('https://i.stack.imgur.com/0Jl54.png');
+  }, []);
+
+  const recognizeTextFromImage = async (path) => {
     console.log('recognizeTextFromImage');
     try {
-      const recognizedText = await TesseractOcr.recognize(
-        path,
-        'hin@eng',
-        {},
-      );
+      const recognizedText = await TesseractOcr.recognize(path, 'hin@eng', {});
       console.log('test is', recognizedText);
       setresultText(recognizedText);
     } catch (error) {
@@ -60,7 +57,7 @@ export default function TessrectModule() {
       const image = await ImagePicker.openPicker(options);
       setimgSrc(image.path);
       setprogress(null);
-       setresultText(null);
+      setresultText(null);
       await recognizeTextFromImage(image.path);
       setimgSrc(image.path);
     } catch (err) {
@@ -71,44 +68,48 @@ export default function TessrectModule() {
   };
   const handleCopy = () => {
     Clipboard.setString(resultText);
-  }
+  };
   return (
     <ScrollView
       style={{
         padding: 20,
-      }}>
-      <Text style={{ marginTop:20, fontSize: 22}}>
+      }}
+    >
+      <Text style={{ marginTop: 20, fontSize: 22 }}>
         Tesseract OCR React Native
       </Text>
       <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
       >
         <Pressable
-           style={styles.featureItem}
-           onPress={() => recognizeFromCamera()}
-        android_ripple={{
-          color: 'cyan',
-        }}
+          style={styles.featureItem}
+          onPress={() => recognizeFromCamera()}
+          android_ripple={{
+            color: 'cyan',
+          }}
         >
-         <Text style={{textAlign:"center", color:"#444", fontSize:14}} >Capture</Text>
-        </Pressable> 
-          <Pressable 
+          <Text style={{ textAlign: 'center', color: '#444', fontSize: 14 }}>
+            Capture
+          </Text>
+        </Pressable>
+        <Pressable
           onPress={() => recognizeFromPicker()}
           style={styles.featureItem}
-        android_ripple={{
-          color: 'cyan'
-        }}
-        > 
-     <Text style={{textAlign:"center", color:"#444", fontSize:14}} >Gellery</Text>
-
+          android_ripple={{
+            color: 'cyan',
+          }}
+        >
+          <Text style={{ textAlign: 'center', color: '#444', fontSize: 14 }}>
+            Gellery
+          </Text>
         </Pressable>
       </View>
       {imgSrc && (
         <Image
-          source={{uri: imgSrc}}
+          source={{ uri: imgSrc }}
           style={{
             width: 200,
             resizeMode: 'contain',
@@ -121,49 +122,52 @@ export default function TessrectModule() {
       )}
       {!resultText && progress ? (
         <View
-        style={{
-          flex:1,
-          justifyContent:"center",
-          alignContent:"center",
-          alignItems:"center",
-          alignSelf:"center",
-        }}
-        >
-          <ActivityIndicator size={'large'} color={'cyan'}  />
-        <Text
           style={{
-            textAlign: 'center',
-            fontSize: 14,
-          }}>Progress - {progress}%</Text></View>
-      ): null}
-      {resultText && (
-        <View 
-        style={{
-          marginBottom:50
-        }}
+            flex: 1,
+            justifyContent: 'center',
+            alignContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'center',
+          }}
         >
-          <Text style={{ textAlign: 'center', fontSize: 22}}>
-            Result
+          <ActivityIndicator size={'large'} color={'cyan'} />
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 14,
+            }}
+          >
+            Progress - {progress}%
           </Text>
+        </View>
+      ) : null}
+      {resultText && (
+        <View
+          style={{
+            marginBottom: 50,
+          }}
+        >
+          <Text style={{ textAlign: 'center', fontSize: 22 }}>Result</Text>
           <Text
             style={{
               backgroundColor: '#fff',
 
-              borderTopRightRadius:8,
-              borderTopLeftRadius:8,
+              borderTopRightRadius: 8,
+              borderTopLeftRadius: 8,
               padding: 20,
               textAlign: 'center',
               fontSize: 14,
-            }}>
+            }}
+          >
             {resultText}
           </Text>
-          <Button 
-          onPress={() => handleCopy()}
-          style={{
-            marginTop: 20,
-            marginLeft:"auto",
-          }}
-          title='Copy Text'
+          <Button
+            onPress={() => handleCopy()}
+            style={{
+              marginTop: 20,
+              marginLeft: 'auto',
+            }}
+            title="Copy Text"
           />
         </View>
       )}
@@ -171,17 +175,15 @@ export default function TessrectModule() {
   );
 }
 
-const styles=StyleSheet.create({
-  featureItem:{
-    marginTop:10,
-    flex:1,
-      padding:10, 
-      paddingVertical:30,
-      borderRadius:10,
-      margin:5,
-      borderRadius:12,
-      flexDirection:"column",
-      alignContent:"center",
-  }
-  
-})
+const styles = StyleSheet.create({
+  featureItem: {
+    marginTop: 10,
+    flex: 1,
+    padding: 10,
+    paddingVertical: 30,
+    margin: 5,
+    borderRadius: 12,
+    flexDirection: 'column',
+    alignContent: 'center',
+  },
+});
